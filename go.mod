f530c7044cd56e3fb069fb2ea1573f93bb61bf5c
Andrew Marcuse <fileformat@gmail.com>
2022-02-12 13:46:26 -0500

    certmagic (untested) and go module files

---
+++ go.mod
@@ -0,0 +1,19 @@
+module github.com/redirect2me/https-proxy
+
+go 1.17
+
+require github.com/caddyserver/certmagic v0.15.2
+
+require (
+	github.com/klauspost/cpuid/v2 v2.0.9 // indirect
+	github.com/libdns/libdns v0.2.1 // indirect
+	github.com/mholt/acmez v1.0.1 // indirect
+	github.com/miekg/dns v1.1.43 // indirect
+	go.uber.org/atomic v1.7.0 // indirect
+	go.uber.org/multierr v1.6.0 // indirect
+	go.uber.org/zap v1.17.0 // indirect
+	golang.org/x/crypto v0.0.0-20210513164829-c07d793c2f9a // indirect
+	golang.org/x/net v0.0.0-20210525063256-abc453219eb5 // indirect
+	golang.org/x/sys v0.0.0-20210423082822-04245dca01da // indirect
+	golang.org/x/text v0.3.6 // indirect
+)
--- main.go
+++ main.go
@@ -2,19 +2,13 @@ package main
 
 import (
 	"flag"
-	//	"fmt"
 	"log"
-	//	"net"
 	"net/http"
 	"net/http/httputil"
 
-	//    "net/url"
 	"os"
-	//    "strconv"
-	//	"strings"
-	//	"sync"
-	//	"sync/atomic"
-	//	"time"
+
+	"github.com/caddyserver/certmagic"
 )
 
 var (
@@ -23,6 +17,7 @@ var (
 	email     = flag.String("email", "r2proxy@mailinator.com", "email address for LetsEncrypt")
 	hostname  = flag.String("hostname", "localhost", "hostname for default server")
 	serveHttp = flag.Bool("http", false, "also handle http on port 80")
+	staging   = flag.Bool("staging", false, "use LetsEncrypt staging server")
 	target    = flag.String("target", "localhost", "target (=proxied server hostname)")
 
 	logger = log.New(os.Stdout, "R2PROXY: ", log.Ldate|log.Ltime|log.Lmicroseconds|log.LUTC)
@@ -66,8 +61,31 @@ func main() {
 	})
 
 	if *serveHttp {
+		logger.Printf("INFO: proxying http on port 80")
 		go http.ListenAndServe(":80", mux)
 	}
 
-	log.Fatal(http.ListenAndServe(*bind, mux))
+	certmagic.DefaultACME.Agreed = true
+	certmagic.DefaultACME.Email = *email
+	if *staging {
+		certmagic.DefaultACME.CA = certmagic.LetsEncryptStagingCA
+	} else {
+		certmagic.DefaultACME.CA = certmagic.LetsEncryptProductionCA
+	}
+	certmagic.Default.OnDemand = &certmagic.OnDemandConfig{
+		DecisionFunc: func(name string) error {
+			//LATER: DNS check
+			//LATER: algorithm check
+			return nil
+		},
+	}
+
+	magic := certmagic.NewDefault()
+	httpsServer := &http.Server{
+		Addr:      *bind,
+		Handler:   mux,
+		TLSConfig: magic.TLSConfig(),
+	}
+
+	log.Fatal(httpsServer.ListenAndServeTLS("", ""))
 }
